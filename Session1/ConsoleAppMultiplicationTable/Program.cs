fb99f89ee85ed13f6999a6d2b147b5b106e6f3fb
PrimssAbdallah <abdallah.anwone@hotmail.com>
2026-04-14 11:11:34 +0000

    Reset des couleurs de la console mise à jour

--- Session1/ConsoleAppMultiplicationTable/Program.cs
+++ Session1/ConsoleAppMultiplicationTable/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Bienvenue dans cette application de simulation de tables de multiplication!");
 Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine("Appuyez sur Entrée pour quitter sans saisir de valeur.");
-Console.ForegroundColor = ConsoleColor.White;
+Console.ResetColor();
 Console.Write("Veuillez entrer un nombre pour lequel vous souhaitez voir la table de multiplication: ");
 var number = Console.ReadLine();
 if (string.IsNullOrEmpty(number))
@@ -30,7 +30,7 @@ while ((start == 0 && end == 0) || start > end)
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Veuillez entrer un nombre valide pour le début de l'intervalle.");
-        Console.ForegroundColor = ConsoleColor.White;
+        Console.ResetColor();
         continue;
     }
 
@@ -48,7 +48,7 @@ while ((start == 0 && end == 0) || start > end)
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Veuillez entrer un nombre valide pour la fin de l'intervalle.");
-        Console.ForegroundColor = ConsoleColor.White;
+        Console.ResetColor();
         continue;
     }
 
@@ -56,7 +56,7 @@ while ((start == 0 && end == 0) || start > end)
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Le nombre de début de l'intervalle doit être inférieur ou égal au nombre de fin.");
-        Console.ForegroundColor = ConsoleColor.White;
+        Console.ResetColor();
     }
 }
 
@@ -67,4 +67,4 @@ for (var i = start; i <= end; i++)
     Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine($"{number} x {i} = {Convert.ToInt16(number) * i}");
 }
-Console.ForegroundColor = ConsoleColor.White;
+Console.ResetColor();
